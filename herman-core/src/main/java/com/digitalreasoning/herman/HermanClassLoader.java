959c2b9ab1d83498572f9d275570c7a6474f0098
Jared Bunting <jared.bunting@peachjean.com>
2013-06-30 22:16:39 -0500

    Making HermanClassLoader more self-contained.

--- herman-core/src/main/java/com/digitalreasoning/herman/HermanClassLoader.java
+++ herman-core/src/main/java/com/digitalreasoning/herman/HermanClassLoader.java
@@ -2,13 +2,16 @@ package com.digitalreasoning.herman;
 
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
+import java.util.Map;
 
 public class HermanClassLoader extends URLClassLoader
 {
 	private final URL isolatedJarLocation;
-	public HermanClassLoader(final URL[] urls, final ClassLoader parent, final URL isolatedJarLocation)
+
+	public HermanClassLoader(final URL[] urls, final ClassLoader parent, final URL isolatedJarLocation, final String[] includes, final String[] excludes)
 	{
-		super(urls, parent);
+		super(urls, new FilteredClassLoader(parent, includes, excludes));
 		this.isolatedJarLocation = isolatedJarLocation;
 	}
 
